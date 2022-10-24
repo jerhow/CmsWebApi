d88e9c9e0f136b92e770142705315c75292b13c5
Jerry Howard <jerryhoward@fastmail.fm>
2022-10-23 18:47:13 -0700

    Remove auto-generated 'weather forecast' files. Set up empty class for operations on courses.

---
+++ Controllers/CoursesController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CmsWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CoursesController : ControllerBase
+    {
+        public CoursesController()
+        {
+
+        }
+
+
+    }
+}
